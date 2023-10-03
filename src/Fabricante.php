165f39c0e4b75d414b8ae965c1eab182ea353ed3
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-10-03 11:52:02 -0300

    criando _construct no arquivo fabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -1,10 +1,21 @@
 <?php
 namespace ExemploCrudPoo;
 
+use PDO;
+
 // final, pois não vai receber herança de ninguem. 
 final class Fabricante {
     private int $id;
     private string $nome;
+
+    /* Essa propriedade receberá os recursos PDO através da classe Banco (dependendo deste projeto) Liga fabricante com banco*/
+    private PDO $conexao;  //crie use linha 4 
+
+    public function __construct(){
+        /* No momento em que um objeto Fabricante for criado, automaticamente será feita a chamada de metodo "conecta" existente na classe Banco. */
+        $this->conexao = Banco::conecta();       
+    }
+
         
     //id
     public function getId(): int
