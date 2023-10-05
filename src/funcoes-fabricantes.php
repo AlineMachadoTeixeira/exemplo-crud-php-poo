2ddfa8eed669701dea610d800cce303d75037ad5
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-10-05 09:47:37 -0300

    excluir Fabricante

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -1,19 +1,6 @@
 <?php
 require_once "conecta.php";
 
-// function lerFabricantes( PDO $conexao ):array {
-//     $sql = "SELECT * FROM fabricantes ORDER BY nome";
-    
-//     try {
-//         $consulta = $conexao->prepare($sql);
-//         $consulta->execute();
-//         $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-//     } catch (Exception $erro) {
-//         die("Erro: ".$erro->getMessage());
-//     }    
-
-//     return $resultado;
-// } 
 
 
 
@@ -23,14 +10,5 @@ require_once "conecta.php";
 
 
 
-function excluirFabricante(PDO $conexao, int $id):void {
-    $sql = "DELETE FROM fabricantes WHERE id = :id";
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
-        $consulta->execute();
-    } catch (Exception $erro) {
-        die("Erro ao excluir: ".$erro->getMessage());
-    }
-} 
+
 
